b36992ab354a8bf807651f8e507e650aa14147f2
GuilhermeEduardo23 <guilhermeeduardo-17@outlook.com>
2023-11-15 15:16:39 +0000

    Finally commit CSS

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,9 @@
+function getYear() {
+    const year = document.getElementById('year');
+    const data = new Date();
+    const ano = data.getFullYear();
 
+    year.innerText = ano;
+}
+
+getYear();
